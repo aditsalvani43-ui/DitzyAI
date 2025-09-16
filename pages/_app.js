4080d30e591225940cf0acffda2945677f505422
aditsalvani43-ui <aditsalvani26@gmail.com>
2025-09-16 14:44:28 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../public/style.css"; // import css global
+import "../public/style.css"; // cuma di sini import CSS global
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
